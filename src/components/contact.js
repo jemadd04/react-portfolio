f18615eb5ca943a13b81462751939188fb1e3f06
James Maddox <james.maddox07@gmail.com>
2019-04-28 00:06:16 -0400

    completed projects and contact pages

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>James Maddox</h2>
+            <img 
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png" 
+              alt="avatar"
+              style={{height: '250px'}}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quia laudantium rem eaque voluptatibus quas in repellat voluptatum, expedita earum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quia laudantium rem eaque voluptatibus quas in repellat voluptatum, expedita earum!</p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                  <i className="fa fa-phone-square" aria-hidden="true" />
+                  (502) 974-8198
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                  <i className="fa fa-envelope" aria-hidden="true" />
+                  james.maddox07@gmail.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                  <i className="fa fa-skype" aria-hidden="true" />
+                  (502) 974-8198
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '25px', fontFamily: 'Anton'}}>
+                  <i className="fa fa-phone-square" aria-hidden="true" />
+                  (502) 974-8198
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
   constructor(props) {
@@ -10,15 +10,53 @@ class Projects extends Component {
   toggleCategories() {
     if(this.state.activeTab === 0){
       return(
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-          <CardTitle style={{color: '#fff', height: '176px', backgroudn: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover'}}>React Project #1</CardTitle>
-          <CardText>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ullam qui aspernatur repellendus assumenda architecto!
-          </CardText>
-          <CardActions>
+        <div className="projects-grid">
+          {/* // Project 1  */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ullam qui aspernatur repellendus assumenda architecto!
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
 
-          </CardActions>
-        </Card>
+          {/* // Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ullam qui aspernatur repellendus assumenda architecto!
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* // Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ullam qui aspernatur repellendus assumenda architecto!
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+
       )
     } else if (this.state.activeTab === 1){
       return(
@@ -45,14 +83,11 @@ class Projects extends Component {
           <Tab>MongoDB</Tab>
         </Tabs>
 
-        <section className="projects-grid">
-          <Grid className="projects-grid">
-            <Cell col={12}>
-              <div className="content">{this.toggleCategories()}</div>
-            </Cell>
-          </Grid>
-          {this.toggleCategories()}
-        </section>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
     )
   }
